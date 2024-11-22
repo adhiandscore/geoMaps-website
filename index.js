61ceb544be198d5dc054f5193b583a25b83509b7
adhiandscore <72400209+adhiandscore@users.noreply.github.com>
2024-11-22 11:09:21 +0700

    update geomaps

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// inisialisasi map, marker & circle
 var map = L.map('maps').setView([51.505, -0.09], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -6,16 +5,43 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-map.on('click', function(event) {
-    // mengambil informasi latitude dan longitude dari event
-    var latitude = event.latlng.lat;
-    var longitude = event.latlng.lng;
-    
-    // tampilkan nilai koordinat
-    console.log("latitude : " + latitude + ", Longitude : " + longitude);
+var coordinates = [];
+
+var polyline = L.polyline([], { color: 'blue' }).addTo(map);
+
+map.on('click', function (event) {
+   
+    var latlng = event.latlng;
+
+    coordinates.push([latlng.lat, latlng.lng]);
 
     // tambahkan marker dan polygon (opsional)
-    L.marker([latitude, longitude]).addTo(map)
-        .bindPopup("Koordinat : " + latitude + ", " + longitude).openPopup();
-    
-})
+    L.marker([event.latlng.lat, event.latlng.lng]).addTo(map)
+        .bindPopup("Koordinat : " + event.latlng.lat + ", " + event.latlng.lng).openPopup();
+
+    polyline.setlatlngs(coordinates);
+
+});
+
+// tombol untuk menggambar polygon
+var drawPolygonButton = L.control({position: 'topright'});
+
+drawPolygonButton.onAdd = function() {
+    var div = L.DomUtil.create('div', 'leaflet-bar leaflet-control leaflet-control-custom');
+    div.innerHTML = '<button style="background: #f4f4f4; padding: 5px; border: none;"> Draw Polygon</button>';
+    div.onclick = function () {
+        // mulai gambar polygon
+        if(coordinates.length > 2) {
+            L.polygon(coordinates, {
+                color:'blue',
+                fillColor:'green',
+                fillOpacity:0.5,
+            }).addTo(map);
+        }else {
+            alert(" Minimal 3 titik untuk bisa menggambar area ");
+        }
+    }; 
+    return div;
+};
+
+drawPolygonButton.addTo(map);
